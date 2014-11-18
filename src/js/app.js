0dc2c0800cf0ce21d06b383180351c6c8d54fcb5
rafael25 <rafaelvb25@gmail.com>
2014-11-18 15:49:05 -0600

    Add fixtures adapter

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 var App = window.App = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
--- src/js/router.js
+++ src/js/router.js
@@ -1,5 +1,6 @@
 App.Router.map(function () {
 	this.resource('recetas', { path: '/recetas' }, function() {
+		this.resource('receta', {path: '/:receta_id'});
 		this.route('new');
 	});
 });
