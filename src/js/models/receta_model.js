2ddc53d114c57b7815cd6c9411eb8260162b91d5
rafael25 <rafaelvb25@gmail.com>
2014-12-08 02:59:00 -0600

    Remove Receta fixtures

--- src/js/models/receta_model.js
+++ src/js/models/receta_model.js
@@ -4,55 +4,9 @@ App.Receta = DS.Model.extend({
 	ingredientes: DS.attr('string'),
 	preparacion: DS.attr('string'),
 	tiempoPrep: DS.attr('number'),
-	rendimiento: DS.attr('string')
+	rendimiento: DS.attr('string'),
+	imagenes: DS.hasMany('imagen', {async: false}),
+	categorias: DS.hasMany('categoria', {async: true})
 });
 
 Ember.Inflector.inflector.irregular('receta', 'recetas');
-
-App.Receta.FIXTURES = [
-	{
-		id: 1,
-		nombre: 'Receta Uno',
-		descripcion: 'Descripcion de la receta uno',
-		ingredientes: 'Los ingredientes\nUn ingrediente\nDos ingedientes',
-		preparacion: 'Estas son las instrucciones de preparacion',
-		tiempoPrep: 25,
-		rendimiento: '2 porciones'
-	},
-	{
-		id: 2,
-		nombre: 'Receta Dos',
-		descripcion: 'Descripcion de la receta Dos',
-		ingredientes: 'Los ingredientes\nUn ingrediente\nDos ingedientes',
-		preparacion: 'Estas son las instrucciones de preparacion',
-		tiempoPrep: 25,
-		rendimiento: '2 porciones'
-	},
-	{
-		id: 3,
-		nombre: 'Receta Tres',
-		descripcion: 'Descripcion de la receta Tres',
-		ingredientes: 'Los ingredientes\nUn ingrediente\nDos ingedientes',
-		preparacion: 'Estas son las instrucciones de preparacion',
-		tiempoPrep: 25,
-		rendimiento: '2 porciones'
-	},
-	{
-		id: 4,
-		nombre: 'Receta Cuatro',
-		descripcion: 'Descripcion de la receta Cuatro',
-		ingredientes: 'Los ingredientes\nUn ingrediente\nDos ingedientes',
-		preparacion: 'Estas son las instrucciones de preparacion',
-		tiempoPrep: 25,
-		rendimiento: '2 porciones'
-	},
-	{
-		id: 5,
-		nombre: 'Receta Cinco',
-		descripcion: 'Descripcion de la receta Cinco',
-		ingredientes: 'Los ingredientes\nUn ingrediente\nDos ingedientes',
-		preparacion: 'Estas son las instrucciones de preparacion',
-		tiempoPrep: 25,
-		rendimiento: '2 porciones'
-	}
-];
