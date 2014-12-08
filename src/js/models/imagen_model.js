a58919866fccaccea9f82cdb6f09a96d03f01c5d
rafael25 <rafaelvb25@gmail.com>
2014-12-08 02:57:32 -0600

    Update Imagen model

--- src/js/models/imagen_model.js
+++ src/js/models/imagen_model.js
@@ -1,1 +1,6 @@
-App.Imagen = DS.Model.extend();
+App.Imagen = DS.Model.extend({
+	url: DS.attr('string'),
+	receta: DS.belongsTo('receta')
+});
+
+Ember.Inflector.inflector.irregular('imagen', 'imagenes');
